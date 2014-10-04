d68873ff695bc2ecfac7d9488d089242e767c4f6
Gil Erlich <gil.erlich@gmail.com>
2014-10-04 14:58:19 +0300

    navigation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $( document ).ready(function() {
+	var currentSectionName;
+
 	$('.app-section').hide();
 	$('.app-section-content').hide();
 
 	$('#' + $('.active .navbar-link').attr('name')).show();
 	$('#' + $('.active .section-menu-link').attr('name') + '-content').show();
+
+	
 
 	$("#example_id").ionRangeSlider({
 	    min: 0,
@@ -20,19 +24,27 @@ $( document ).ready(function() {
 		var	$current = $(".active .navbar-link"),
 			currentName = $current.attr('name'),
 			newName = $(this).attr('name');
-
-			
+	
+		// hide current section
 		$current.parent().removeClass('active');
 		$('#' + currentName).hide();
 
+		// show the new section
 		$(this).parent().addClass('active');
 		$('#' + newName).show();		
+
+		// show the currently active contents of the new section
+		$('#' + $('.active .'+ newName + '-section-menu-link').attr('name') + '-content').show();
+
+		currentSectionName = newName;
 	});
 
 	$(".section-menu-link").click(function() {
-		var	$current = $(".active .section-menu-link"),
+		var	$current = $(".active ." + currentSectionName + "-section-menu-link"),
 			currentName = $current.attr('name'),
 			newName = $(this).attr('name');
+
+		console.log(currentSectionName, currentName, newName);
 
 		$current.parent().removeClass('active');
 		$('#' + currentName + '-content').hide();
